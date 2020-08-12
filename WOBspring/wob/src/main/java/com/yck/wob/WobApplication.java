8bbe062484094c17fcf684951e33fbd02ce1d1e0
JinDogNOOB <mrriey00@naver.com>
2020-08-12 16:42:48 +0900

    gen/springprojectfile

---
+++ WOBspring/wob/src/main/java/com/yck/wob/WobApplication.java
@@ -0,0 +1,13 @@
+package com.yck.wob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WobApplication.class, args);
+	}
+
+}
---
+++ WOBspring/wob/src/test/java/com/yck/wob/WobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yck.wob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
