283201d25bfe5e0745f05c6cb0f05bc72590a952
JinDogNOOB <mrriey00@naver.com>
2020-10-28 17:08:37 +0900

    add - i made button to show adminfunctions section on AdminPage.js and etc

--- reactSpa/wobspa/src/components/AdminFunction.js
+++ reactSpa/wobspa/src/components/AdminFunction.js
@@ -9,13 +9,14 @@ export const REQUESTEDBOARD = 3;
 
 
 
-function AdminFunction(){
+function AdminFunction({mode, onSetMode}){
     // admin menu 가 무엇이 눌렀느냐에 따라서 
     // amdin info 가 그려져야한다... 흠 생각생각하즈아아
 
     let adminInfoJSX = (<div>null</div>);
 
-
+    /*
+    테스트 코드임 
     let testArray = ['a', 'b', 'c', 'd'];
     if(true){
         adminInfoJSX = 
@@ -24,18 +25,19 @@ function AdminFunction(){
                     <h1>{data}</h1>
                 </div>
             ));
-    }
-    switch(1){
-        case USER:{
+    } */
 
+    switch(mode){
+        case USER:{
+            adminInfoJSX = (<div>userMode</div>);
             break;
         }
         case BOARD:{
-
+            adminInfoJSX = (<div>boardMode</div>);
             break;
         }
         case REQUESTEDBOARD:{
-
+            adminInfoJSX = (<div>requestedBoardMode</div>);
             break;
         }
         default:{
@@ -43,9 +45,13 @@ function AdminFunction(){
         }
     }
 
+    
     return (
         <div className="admin_function_container">
             <div className="admin_menu">
+                <a href="#" onClick={() => onSetMode(USER)}>유저관리</a>
+                <a href="#" onClick={() => onSetMode(BOARD)}>게시판관리</a>
+                <a href="#" onClick={() => onSetMode(REQUESTEDBOARD)}>게시판생성요청관리</a>
 
             </div>
             <div className="admin_info">
--- reactSpa/wobspa/src/containers/AdminFunctionContainer.js
+++ reactSpa/wobspa/src/containers/AdminFunctionContainer.js
@@ -6,9 +6,18 @@ import AdminFunction from '../components/AdminFunction';
 
 
 function AdminFunctionContainer(){
+    // 버튼 눌린 상태 저장 defualt 유저버튼
+    const [mode, setMode] = useState(1);
+
+    const onSetMode = (i) => {
+        setMode(i);
+    }
 
     return (
-        <AdminFunction />
+        <AdminFunction 
+        mode = {mode}
+        onSetMode = {onSetMode}
+        />
     );
 }
 
