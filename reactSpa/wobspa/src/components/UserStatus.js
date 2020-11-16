e9b92cca663ba19808f2666a48c3bc544ad7b7e6
JinDogNOOB <mrriey00@naver.com>
2020-11-16 17:13:29 +0900

    refac i've detached signin,up function from UserStatus

--- reactSpa/wobspa/src/components/UserStatus.js
+++ reactSpa/wobspa/src/components/UserStatus.js
@@ -9,140 +9,30 @@ import './Component.css';
 로그인 : ~~님 환영합니다 로그아웃
 
 */
-function UserStatus({onSetIsLoggedInStatus, isLoggedIn, onRequestSignup, onRequestLogin}){
+function UserStatus({
+    isLoggedIn,
+    onClickGotoSigninPage,
+    onClickGotoSignupPage,
+    onClickLogOut
+}){
     //var button = document.querySelector("#loginButton");
     //button.addEventListener(modalSwitch)
     // 모달 레퍼런스
-    const signinModalRef = React.createRef();
-    const signupModalRef = React.createRef();
-
-    // 로그인폼 상태
-    const [userId, setUserId] = useState("");
-    const [userPassword, setUserPassword] = useState("");
-    // 회원가입폼에 따른 추가 상태
-    const [userNickname, setUserNickname] = useState("");
-
-    // 상태 변경 함수
-    const updateUserId = (event) => {
-        setUserId(event.target.value);
-    }
-    const updateUserPassword = (event) => {
-        setUserPassword(event.target.value);
-    }
-    const updateUserNickname = (event) => {
-        setUserNickname(event.target.value);
-    }
-
-    console.log("rerender");
-
-
-    // 사용함수 
-    // 회원가입 로그인 모달 스위치
-    const modalSwitch = (modal) =>{
-        // var modal = document.querySelector(".modal");
-        modal.current.classList.toggle("modal-switch");
-    };
-    // 회원가입 후에 실행할 함수 
-    const doWhenSignupEnd = (bool) => {
-        if(bool){
-            modalSwitch(signupModalRef);
-            alert("회원가입이 완료되었습니다!!!");
-        }else{
-            console.log("회원가입실패");
-            alert("중복된 이메일이 존재하여 회원가입에 실패했습니다");
-        }
-    }
-    // 로그인 후에 실행할 함수
-    const doWhenSigninEnd = (bool) => {
-        if(bool){
-            modalSwitch(signinModalRef);
-            alert("로그인에 성공했습니다!");
-            onSetIsLoggedInStatus(true);
-        }else{
-             console.log("로그인 실패");
-             alert("로그인에 실패했습니다");
-
-        }
-    }
-    // 아 이거를 조금 그 위로 뺄수는없을까..
-
-
-
 
     if(!isLoggedIn){
         return(
-            <div>
+            <div className="UserStatus">
                 {/*  로그인이 안되있을 시  */}
-                <div className="UserStatus">
-                    <button onClick={() => modalSwitch(signinModalRef)}>로그인</button>
-                    <button onClick={() => modalSwitch(signupModalRef)}>회원가입</button>
-                </div>
-
-            
-                {/* 
-                모달 출처http://cosmosjs.blogspot.com/2018/11/css.html
-                로그인 회원가입 출력될 모달
-                */}
-                {/*  로그인  */}
-                <div className="modal" ref={signinModalRef} >
-                    <div className="modal-content">
-
-                        <div className="container-flex direction-col">
-                            <h1>로그인</h1>
-                            <div className="container-flex justifyContent-center">
-                                <h2>아이디</h2>
-                                <input type="text" value={userId} onChange={updateUserId}></input>
-                            </div>
-                            <div className="container-flex justifyContent-center">
-                                <h2>비밀번호</h2>
-                                <input type="text" value={userPassword} onChange={updateUserPassword}></input>
-                            </div>
-                            <div className="container-flex justifyContent-center">
-                                <input type="button" className="modal-button" onClick={() => modalSwitch(signinModalRef)} value="취소" />
-                                <input type="button" className="modal-button" value="확인" onClick={() => onRequestLogin(userId, userPassword, (bool) => doWhenSigninEnd(bool))} />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                {/*  회원가입  */}
-                <div className="modal" ref={signupModalRef} >
-                    <div className="modal-content">
-                        
-                        <div className="container-flex direction-col">
-                            <h1>회원가입</h1>
-                            <div className="container-flex justifyContent-center">
-                                <h2>아이디</h2>
-                                <input type="text" value={userId} onChange={updateUserId}></input>
-                            </div>
-                            <div className="container-flex justifyContent-center">
-                                <h2>비밀번호</h2>
-                                <input type="text" value={userPassword} onChange={updateUserPassword}></input>
-                            </div>
-                            <div className="container-flex justifyContent-center">
-                                <h2>닉네임</h2>
-                                <input type="text" value={userNickname} onChange={updateUserNickname}></input>
-                            </div>
-                            <div className="container-flex justifyContent-center">
-                                <input type="button" onClick={() => modalSwitch(signupModalRef)} value="취소" />
-                                <input type="button" onClick={() => onRequestSignup(userId, userPassword, userNickname, (bool) => doWhenSignupEnd(bool))} value="확인" />
-                            </div>
-                        </div>
-
-
-
-                    </div>
-                </div>
-
+                <button onClick={() => {onClickGotoSigninPage()}}>로그인</button>
+                <button onClick={() => {onClickGotoSignupPage()}}>회원가입</button>
             </div>
-        );
+        )
     }else{
         return(
             <div className="UserStatus">
                 <div className="UserStatusDiv">
                     님환영 님이 무엇입니까?!!
-                    <input type="button" onClick={() => onSetIsLoggedInStatus(false)} value="로그아웃" />
+                    <input type="button" value="로그아웃" onClick={()=>{onClickLogOut()}}/>
                 </div>
             </div>
         );
--- reactSpa/wobspa/src/containers/UserStatusContainer.js
+++ reactSpa/wobspa/src/containers/UserStatusContainer.js
@@ -6,14 +6,14 @@ import axiosOptions from '../tool/axiosOptions';
 import {setLoginStatus, setJwt} from '../modules/user';
 import UserStatus from '../components/UserStatus';
 
+import {useHistory} from 'react-router-dom';
 
 
 
 
 
 function UserStatusContainer(){
-
-    console.log("container rerender");
+    const history = useHistory();
     // redux store
     const {isLoggedIn} = useSelector(state=> ({
         isLoggedIn: state.user.isLoggedIn
@@ -23,69 +23,23 @@ function UserStatusContainer(){
     }));
 
     const dispatch = useDispatch();
-    const onSetIsLoggedInStatus = (bool) => dispatch(setLoginStatus(bool));
 
-    // axios
-    // 회원가입
-    const onRequestSignup = async(userEmail, userPassword, userNickname, callback) => {
-        try{
-            const response = await axios(axiosOptions.put('/user/', {
-                userEmail : userEmail,
-                userPassword : userPassword,
-                userNickname : userNickname
-            }));
-            console.log(response);
-            if(response.status / 100 == 2){
-                callback(true);
-                //성공
-                // 아 axios 400뜨면 exception발생시키는구나 
-            }else{
-                // 100 300 500 아 500은 excepiton 빠질라나
-                callback(false);
-            }
-        }catch(exception){
-            callback(false);
-            console.log(exception);
-        }
-    };
-    // axios
-    // 로그인
-    const onRequestLogin = async(userEmail, userPassword, callback) => {
-        try{
-            const response = await axios(axiosOptions.post('/user/', {
-                userEmail : userEmail,
-                userPassword : userPassword
-            }));
-            if(response.status / 100 == 2){
-                callback(true);
-                // console.log(response.data.jwt);
-                dispatch(setJwt(response.data.jwt));
-                //성공
-            }else{
-                callback(false);
-                // 실패
-            }
-
-        }catch(exception){
-            // http status 400대일때 일로 빠지네,,.
-            callback(false);
-            console.log(exception);
-        }
+    // common
+    const onClickGotoSigninPage = () => {history.push('/user/signin');}
+    const onClickGotoSignupPage = () => {history.push('/user/signup');}
+    const onClickLogOut = () => {
+        dispatch(setJwt(""));
+        dispatch(setLoginStatus(false));
     }
 
-
-
-
+    // 로그인 유효 체크 반복이있어야할듯 여기에
     return(
-        <div>
             <UserStatus 
-            onSetIsLoggedInStatus = {onSetIsLoggedInStatus}
             isLoggedIn = {isLoggedIn}
-            onRequestSignup = {onRequestSignup}
-            onRequestLogin = {onRequestLogin}
+            onClickGotoSigninPage = {onClickGotoSigninPage}
+            onClickGotoSignupPage = {onClickGotoSignupPage}
+            onClickLogOut = {onClickLogOut}
             />
-
-        </div>
     );
 
 }
