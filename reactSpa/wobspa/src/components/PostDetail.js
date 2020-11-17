7a209ee1108fa01f2c8ef37676f0d9e592b726d2
JinDogNOOB <mrriey00@naver.com>
2020-11-17 16:31:01 +0900

    add subPost

--- reactSpa/wobspa/src/components/PostDetail.js
+++ reactSpa/wobspa/src/components/PostDetail.js
@@ -6,8 +6,15 @@ function PostDetail({
     postName,
     postDesc,
     onClickCancel,
-    onClickModify
+    onClickModify,
+
+    subPostList,
+    subPostDesc,
+    onSetSubPostDesc,
+    onInsertSubPost
 }) { 
+
+
 
     return (
         <div>
@@ -20,8 +27,55 @@ function PostDetail({
                 <input type="button" value="취소" onClick={() => {onClickCancel()}} />
             </div>
 
+            <SubPost 
+            subPostList = {subPostList}
+            subPostDesc = {subPostDesc}
+            onSetSubPostDesc = {onSetSubPostDesc}
+            onInsertSubPost = {onInsertSubPost}
+            />
+
+
         </div>
     );
 }
+
+function SubPost({
+    subPostList,
+    subPostDesc,
+    onSetSubPostDesc,
+    onInsertSubPost
+}){
+        const SubPostList = () => {
+            return (
+            <div>
+                {subPostList.map((val, index) => (
+                    <tr>
+                        <td>{val.postSubNo}</td>
+                        <td>{val.postSubDesc}</td>
+                        <td>{val.postSubOwner}</td>
+                    </tr>
+                ))}
+            </div>
+            )
+
+        }
+        
+        return(
+            <div>
+                <div>
+                    <input type="text" value={subPostDesc} onChange={onSetSubPostDesc} />
+                    <input type="button" value="댓글작성" onClick={() => {onInsertSubPost()}} />
+                </div>
+                <div>
+                    <SubPostList />
+                </div>
+            </div>
+
+        );
+}
+
+
+
+
 
 export default PostDetail;
--- reactSpa/wobspa/src/containers/PostDetailContainer.js
+++ reactSpa/wobspa/src/containers/PostDetailContainer.js
@@ -19,8 +19,15 @@ function PostDetailContainer({boardNo, postNo}){
     }));
 
     // state
+    // 본글
     const [postName, setPostName] = useState("");
     const [postDesc, setPostDesc] = useState("");
+    // 댓글 입력, 수정용
+    const [subPostDesc, setSubPostDesc] = useState("");
+    const onSetSubPostDesc = (e) => {setSubPostDesc(e.target.value)}
+    // 댓글 리스트
+    const [subPostList, setSubPostList] = useState([]);
+    
 
     // common 
     const onCancel = () => {
@@ -43,10 +50,69 @@ function PostDetailContainer({boardNo, postNo}){
         }
     }
 
+    // 댓글 가져오기
+    const onLoadSubPost = async() => {
+        try{
+            const response = await axios(axiosOptions.get('/board/'+boardNo+'/post/'+postNo+'/sub', {
+            }));
+            let tempArray = [];
+            response.data.map((val, index) => {
+                let temp = {
+                    postSubNo : val.postSubNo,
+                    postSubDesc : val.postSubDesc,
+                    postSubOwner : val.postSubOwner
+                }
+                tempArray.push(temp);
+            });
+            setSubPostList(tempArray);
+
+        }catch(e){
+            console.log(e);
+        }
+    }
+    // 댓글 쓰기
+    const onInsertSubPost = async() => {
+        try{
+            const response = await axios(axiosOptions.post('/board/'+boardNo+'/post/'+postNo+'/sub', {
+                postSubDesc : subPostDesc,
+                jwt : jwt
+            }));
+            setSubPostDesc("");
+            onLoadSubPost();
+        }catch(e){
+            console.log(e);
+        }
+    }
+    // 댓글 수정 XXXXXXXXXXXXXX 수정 ㄴㄴㄴ 아니다
+    // 수정이 필요없다 삭제만 넣자
+    const onModifySubPost = async(postSubNo) => {
+        try{
+            const response = await axios(axiosOptions.put('/board/'+boardNo+'/post/'+postNo+'/sub/'+postSubNo, {
+                postSubDesc : subPostDesc,
+                jwt : jwt
+            }));
+            setSubPostDesc("");
+        }catch(e){
+            console.log(e);
+        }
+    }
+    // 댓글 삭제
+    const onDeleteSubPost = async(postSubNo) => {
+        try{
+            const response = await axios(axiosOptions.delete('/board/'+boardNo+'/post/'+postNo+'/sub/'+postSubNo, {
+                jwt : jwt
+            }));
+
+        }catch(e){
+            console.log(e);
+        }
+    }
+
     
     // useEffect
     useEffect(() => {
         onLoadPost();
+        onLoadSubPost();
     }, []);
     
 
@@ -56,6 +122,13 @@ return (
         postDesc = {postDesc}
         onClickCancel = {onCancel}
         onClickModify = {gotoModifyingPage}
+
+        subPostList = {subPostList}
+        subPostDesc = {subPostDesc}
+        onSetSubPostDesc = {onSetSubPostDesc}
+        onInsertSubPost = {onInsertSubPost}
+        
+
     />
 );
 
