5e1173bff99912f38accefd1c4ddb322711c74bd
JinDogNOOB <mrriey00@naver.com>
2020-12-25 18:53:55 +0900

    apply lombok & change userStatus data type to UserRoleType. enum class

--- back/wob/src/main/java/com/yck/wob/dto/UserDTO.java
+++ back/wob/src/main/java/com/yck/wob/dto/UserDTO.java
@@ -1,50 +1,14 @@
 package com.yck.wob.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserDTO {
     private int userNo;
     private String userPassword;
     private String userEmail;
     private String userNickname;
     private int userStatus;
-
-    public int getUserNo() {
-        return userNo;
-    }
-
-    public void setUserNo(int userNo) {
-        this.userNo = userNo;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getUserNickname() {
-        return userNickname;
-    }
-
-    public void setUserNickname(String userNickname) {
-        this.userNickname = userNickname;
-    }
-
-    public int getUserStatus() {
-        return userStatus;
-    }
-
-    public void setUserStatus(int userStatus) {
-        this.userStatus = userStatus;
-    }
-
 }
